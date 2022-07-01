edd72c477ea58d957554a133d9878cd6775c962a
pedpersil <pedpersil@gmail.com>
2022-07-01 18:57:10 -0300

    App Send Mail com PHPMailer

--- processa_envio.php
+++ processa_envio.php
@@ -1,10 +1,10 @@
 <?php
 
-	require "bibliotecas/PHPMailer/Exception.php";
-	require "bibliotecas/PHPMailer/OAuth.php";
-	require "bibliotecas/PHPMailer/PHPMailer.php";
-	require "bibliotecas/PHPMailer/POP3.php";
-	require "bibliotecas/PHPMailer/SMTP.php";
+	require "lib/PHPMailer/Exception.php";
+	require "lib/PHPMailer/OAuth.php";
+	require "lib/PHPMailer/PHPMailer.php";
+	require "lib/PHPMailer/POP3.php";
+	require "lib/PHPMailer/SMTP.php";
 
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
